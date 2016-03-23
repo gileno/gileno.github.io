a1905478f5ca122da10934077d6c7b54750b416b
Gileno Filho <gascf.cin@gmail.com>
2016-03-23 10:20:04 -0300

    adicionando fabfile

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://wwww.gilenofilho.com.br'
+SITEURL = 'http://www.gilenofilho.com.br'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds.atom'
